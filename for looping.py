27ab8434beeb06a08e208cc5d19c1265774ece9b
MueenMahmud <muhinmahmud1105@gmail.com>
2025-03-07 22:49:40 +0600

    Create for looping.py
    for looping test

---
+++ for looping.py
@@ -0,0 +1,9 @@
+person = {'name': "Mueein", 'age ': 25, 'city': 'Dhaka'}
+'''for loop in person:
+    print(loop)
+    
+for value in person.values():
+    print(value)'''
+    
+for loop, value in person.items():
+    print(loop, ":", value)
